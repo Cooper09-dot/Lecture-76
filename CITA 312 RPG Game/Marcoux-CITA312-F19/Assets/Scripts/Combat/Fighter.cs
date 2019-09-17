6a2c7a1cccaeaf3a77082269b7f46cd9395d6e39
Jermaine09 <cooper906@morrisville.edu>
2019-09-17 10:19:53 -0400

    S5L37

---
+++ CITA 312 RPG Game/Marcoux-CITA312-F19/Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using RPG.Movement;
+using RPG.Core;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour, IAction
+    {
+        [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
+        [SerializeField] float weaponDamage = 5f;
+
+        Transform target;
+        float timeSinceLastAttack = 0;
+
+        private void Update()
+        {
+            timeSinceLastAttack += Time.deltaTime;
+
+            if (target == null) return;
+
+            // move only if a target is NOT within range
+           if (!GetIsInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else // in range, attack!
+            {
+                GetComponent<Mover>().Cancel();
+                AttackBehavior();
+            }
+        }
+
+        private void AttackBehavior()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                // This will trigger the Hit() event
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0f;
+                
+            }
+            
+        }
+
+        // this is an animation event
+        void Hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
+        }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            target = combatTarget.transform;
+        }
+
+        public void Cancel()
+        {
+            target = null;
+        }
+
+    } // class Fighter
+} // namespace
